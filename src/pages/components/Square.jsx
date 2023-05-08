98926c94d2357782fb70f387ac52cf8df9d777bc
Federico Villagra <federico.villagra@mi.unc.edu.ar>
2023-05-08 14:26:00 -0300

    vuelvo todo como estaba

--- src/pages/components/Square.jsx
+++ src/pages/components/Square.jsx
@@ -25,39 +25,21 @@ const Square = (props) => {
       e.dataTransfer.setData('number', props.i)
   }
   const drop = (e) => {
-    e.preventDefault();
     const value = e.dataTransfer.getData('text/plain');
     const fromIndex = parseInt(e.dataTransfer.getData('number'), 10);
     const toIndex = props.i;
-  
-    // Si es un dispositivo táctil
-    if (e.type === 'touchend') {
-      // Obtener las coordenadas de la posición actual del dedo
-      const clientX = e.changedTouches[0].clientX;
-      const clientY = e.changedTouches[0].clientY;
-  
-      // Buscar el elemento en la página que se encuentre debajo del dedo
-      const element = document.elementFromPoint(clientX, clientY);
-      
-      // Si el elemento es el botón y es un movimiento válido
-      if (element === e.target && canMoveToSquare(fromIndex, toIndex) && !props.winner && props.squares[toIndex] == null && (value === 'X') === props.xIsNext) {
+    //si no es un movimiento valido o hay ganador o el cuadrado esta ocupado o no es el turno del jugador, no hace nada.
+    console.log(value);
+    console.log(value==='X');
+    console.log(props.xIsNext);
+    if (!canMoveToSquare(fromIndex, toIndex) || props.winner || props.squares[toIndex] || (value==='X') !== props.xIsNext) {
+      return;
+    }
         const newSquares = [...props.squares];
         newSquares[toIndex] = value;
         newSquares[fromIndex] = null;
         props.setSquares(newSquares);
-        props.setXIsNext(!props.xIsNext);
-      }
-    } else {
-      // Si no es un dispositivo táctil, seguir usando el evento drop original
-      if (!canMoveToSquare(fromIndex, toIndex) || props.winner || props.squares[toIndex] || (value==='X') !== props.xIsNext) {
-        return;
-      }
-      const newSquares = [...props.squares];
-      newSquares[toIndex] = value;
-      newSquares[fromIndex] = null;
-      props.setSquares(newSquares);
-      props.setXIsNext(!props.xIsNext);
-    }
+        props.setXIsNext(!props.xIsNext); //cambia el turno
   }
 
   return (
@@ -65,12 +47,9 @@ const Square = (props) => {
     className={styles.square} 
     onClick={props.onClick} 
     draggable={isDraggable()}
-    onDragStart={dragstart}
+    onDragStart={dragstart} //pasa data de la pieza levantada
     onDragOver={(e) =>e.preventDefault()}
-    onDrop={drop} 
-    onTouchStart={drop} 
-    onTouchEnd={drop}
-    onTouchMove={drop}
+    onDrop={drop} //mover la pieza
     >
       {props.value === 'X' && <div className={styles.x}>X</div>}
       {props.value === 'O' && <div className={styles.o}>O</div>}
